e59e792a0a81621c12d93840e2674eb40ba09268
lundrew <andrueee@gmail.com>
2022-05-13 15:44:15 -0700

    Added cat attributes to cat_spec

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
   it "should validate name" do
-    cat = Cat.create
+    cat = Cat.create age: 5, enjoys: 'walking around the park', image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
     expect(cat.errors[:name]).to_not be_empty
   end
   it "should validate age" do
-    cat = Cat.create
+    cat = Cat.create name: "Duke", enjoys: 'walking around the park', image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
     expect(cat.errors[:age]).to_not be_empty
   end
   it "should validate enjoys" do
-    cat = Cat.create 
+    cat = Cat.create name: "Duke", age: 5, image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
     expect(cat.errors[:enjoys]).to_not be_empty
   end
   it "should validate enjoys is more than 10 characters" do
@@ -18,7 +18,7 @@ RSpec.describe Cat, type: :model do
     expect(cat.errors[:enjoys]).to_not be_empty
   end
   it "should validate image" do
-    cat = Cat.create
+    cat = Cat.create name: "Duke", age:5, enjoys: 'walking around the park' 
     expect(cat.errors[:image]).to_not be_empty
   end
 end
